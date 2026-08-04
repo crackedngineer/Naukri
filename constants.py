56ae6b17716ec8477404da057d77a406db60d915
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2026-08-04 18:08:57 +0530

    Refactor: update cron workflow and adjust resume file path in constants

--- constants.py
+++ constants.py
@@ -3,7 +3,7 @@
 USERNAME = "your_naukri_username"
 PASSWORD = "your_naukri_password"
 MOBILE = "your_naukri_phone"
-ORIGINAL_RESUME_PATH="your-resume-file-path/Resume.pdf"
+ORIGINAL_RESUME_PATH="resume.pdf"
 MODIFIED_RESUME_PATH="modified-resume-file-path/Resume.pdf"
 NAUKRI_LOGIN_URL="https://www.naukri.com/nlogin/login"
 NAUKRI_PROFILE_URL="https://www.naukri.com/mnjuser/profile"
